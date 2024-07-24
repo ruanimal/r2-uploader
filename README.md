4974632782ea3f6d53126f49ad65b7405b87b6d0
ruan.lj <ruan.lj@foxmail.com>
2024-07-24 16:27:42 +0800

    first version

---
+++ README.md
@@ -0,0 +1,25 @@
+# Usage
+## Run Server
+uvicorn uploader:app
+
+## Nginx Config (Optional)
+```
+server {
+    underscores_in_headers on;
+
+    location /upload {
+        proxy_pass         http://127.0.0.1:8000;
+        proxy_set_header Host $host;
+    }
+}
+```
+
+## Upload Image
+args:
+    account_id: required
+    access_key_id: required
+    secret_access_key: required
+    bucket: required
+    file: required
+    key_pattern: optional
+    url_prefix: optional
---
+++ uploader.py
@@ -0,0 +1,61 @@
+from typing import Union, Annotated
+from botocore.client import Config
+import boto3
+
+from datetime import datetime
+from fastapi import FastAPI, Header, Form, UploadFile, Request, File
+
+
+app = FastAPI()
+StrForm = Annotated[str, Form()]
+StrHeader = Annotated[str, Header(convert_underscores=False)]
+
+def get_s3agent(account_id, access_key_id, secret_access_key, region='auto'):
+    s3 = boto3.client('s3',
+                    endpoint_url=f'https://{account_id}.r2.cloudflarestorage.com',
+                    aws_access_key_id=access_key_id,
+                    aws_secret_access_key=secret_access_key,
+                    config=Config(signature_version='s3v4'),
+                    region_name=region)
+    return s3
+
+def upload_r2(s3, content: bytes, bucket: str, key: str):
+    res = s3.put_object(Body=content, Bucket=bucket, Key=key)
+    return res
+
+@app.post("/upload")
+async def upload(account_id: StrHeader, access_key_id: StrHeader, secret_access_key: StrHeader,
+           bucket: StrHeader, host: Annotated[str, Header()],
+           file: UploadFile = File(...), key_pattern: StrForm = '', url_prefix: StrForm = '',
+           ):
+    s3 = get_s3agent(account_id, access_key_id, secret_access_key)
+    now = datetime.now()
+    if not key_pattern:
+        key_pattern = 'mweb/{year:0>4}-{mon:0>2}-{day:0>2}---{filename}'
+
+    args = dict(
+        year=now.year,
+        mon=now.month,
+        day=now.day,
+        hour=now.hour,
+        filename=file.filename,
+    )
+    key = key_pattern.format(**{k: v for k, v in args.items() if '{'+k in key_pattern})
+    if not url_prefix:
+        url_prefix = f'https://{host}'
+
+    content = file.file.read()
+    resp = upload_r2(s3, content, bucket, key)
+    if resp['ResponseMetadata']['HTTPStatusCode'] != 200:
+        return {'code': -1, 'msg': 'upload failed!', 'data': resp}
+
+    res = {
+        "code": 0,
+        "data": {
+            "size": file.size,
+            "path": key,
+            "url": f'{url_prefix}/{key}',
+        }
+    }
+    return res
+
